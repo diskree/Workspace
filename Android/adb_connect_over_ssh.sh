5c7207658553fe9c844a92d342b6a7b4a45a9632
diskree <diskriminant.was.taken@gmail.com>
2023-03-08 08:27:37 +0400

    Update local IP address

--- Android/adb_connect_over_ssh.sh
+++ Android/adb_connect_over_ssh.sh
@@ -4,4 +4,4 @@ sleep 1
 /data/data/com.termux/files/usr/bin/su -c "settings put global adb_wifi_enabled 1"
 sleep 1
 ADB_WIFI_PORT=$(/data/data/com.termux/files/usr/bin/su -c "getprop service.adb.tls.port")
-/data/data/com.termux/files/usr/bin/sshpass -p "++++" /data/data/com.termux/files/usr/bin/ssh "diskree-hyperpc\diskree@192.168.1.100" 'wsl -e bash -lic "/home/diskree-wsl/Android/Sdk/platform-tools/adb connect 192.168.1.102:'"${ADB_WIFI_PORT}"'"'
+/data/data/com.termux/files/usr/bin/sshpass -p "++++" /data/data/com.termux/files/usr/bin/ssh "diskree-hyperpc\diskree@192.168.1.100" 'wsl -e bash -lic "/home/diskree-wsl/Android/Sdk/platform-tools/adb connect 192.168.1.101:'"${ADB_WIFI_PORT}"'"'
