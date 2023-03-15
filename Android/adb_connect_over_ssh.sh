a93bf572432b63a0c6332fba1d0eeb6905d8f9f5
diskree <diskriminant.was.taken@gmail.com>
2023-03-16 01:04:12 +0400

    Config file

--- Android/adb_connect_over_ssh.sh
+++ Android/adb_connect_over_ssh.sh
@@ -1,7 +1,7 @@
-#!/data/data/com.termux/files/usr/bin/sh
 /data/data/com.termux/files/usr/bin/su -c "settings put global adb_wifi_enabled 0"
 sleep 1
 /data/data/com.termux/files/usr/bin/su -c "settings put global adb_wifi_enabled 1"
 sleep 1
 ADB_WIFI_PORT=$(/data/data/com.termux/files/usr/bin/su -c "getprop service.adb.tls.port")
-/data/data/com.termux/files/usr/bin/sshpass -p "++++" /data/data/com.termux/files/usr/bin/ssh "diskree-hyperpc\diskree@192.168.1.100" 'wsl -e bash -lic "/home/diskree-wsl/Android/Sdk/platform-tools/adb connect 192.168.1.101:'"${ADB_WIFI_PORT}"'"'
+. /data/data/com.termux/files/home/.scripts/config
+/data/data/com.termux/files/usr/bin/sshpass -p "$PC_PASSWORD" /data/data/com.termux/files/usr/bin/ssh "${PC_HOST}@${PC_IP}" 'adb connect '"${OP9PRO_IP}"':'"${ADB_WIFI_PORT}"''
--- Android/wol_workspace.sh
+++ Android/wol_workspace.sh
@@ -1,3 +1,2 @@
-#!/data/data/com.termux/files/usr/bin/sh
-/data/data/com.termux/files/usr/bin/wol --host 192.168.1.100 --ipaddr 192.168.1.255 D8:5E:D3:59:43:DB
-/data/data/com.termux/files/usr/bin/wol --host 192.168.1.101 --ipaddr 192.168.1.255 00:D8:61:E1:0F:46
+. /data/data/com.termux/files/home/.scripts/config
+/data/data/com.termux/files/usr/bin/wol --host $PC_IP --ipaddr $ROUTER_IP $PC_MAC
