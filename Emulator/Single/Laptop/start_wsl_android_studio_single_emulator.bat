d5b12e80a9be8a60e0cca7ddd18444e506dd90c8
diskree <sergei.grushin@imem.app>
2023-04-20 06:35:54 +0400

    Removed laptop

--- Emulator/Single/Laptop/start_wsl_android_studio_single_emulator.bat
+++
@@ -1,5 +0,0 @@
-call D:\Workspace\Windows\stop.bat
-wscript.exe "D:\Workspace\Common\invisible.vbs" "D:\Workspace\Emulator\start_33_emulator.bat"
-start /wait D:\AHK\AutoHotkey.exe "D:\Workspace\Emulator\wait_33_emulator.ahk"
-start D:\AHK\AutoHotkey.exe "D:\Workspace\Emulator\toggle_android_studio_emulator_panels.ahk"
-start /wait D:\AHK\AutoHotkey.exe "D:\Workspace\Emulator\hide_emulator_menus.ahk"
